146810987303819d2cb500209af9624207566657
m-gerber <64030957+m-gerber@users.noreply.github.com>
2023-09-15 12:08:39 -0600

    added functions for goalReached, nearPolygon, and nearWall

--- ws/MyBugAlgorithm.cpp
+++ ws/MyBugAlgorithm.cpp
@@ -1,14 +1,209 @@
 #include "MyBugAlgorithm.h"
+#include <thread>
 
 // Implement your methods in the `.cpp` file, for example:
 amp::Path2D MyBugAlgorithm::plan(const amp::Problem2D& problem) const {
 
-    // Your algorithm solves the problem and generates a path. Here is a hard-coded to path for now...
+    // Your algorithm solves the problem and generates a path. Here is a path definition.
     amp::Path2D path;
-    path.waypoints.push_back(problem.q_init);
-    path.waypoints.push_back(Eigen::Vector2d(1.0, 5.0));
-    path.waypoints.push_back(Eigen::Vector2d(3.0, 9.0));
-    path.waypoints.push_back(problem.q_goal);
+
+    // Initialize the starting point for the bug to be the intial point for the bug.
+    double x_bug = problem.q_init[0];
+    double y_bug = problem.q_init[1];
+
+    // Create a vector of type PathPoints to hold x, y, coordinates of steps, step sizes, and distance to goal.
+    std::vector<amp::PathPoints> bug_path;
+
+    // Define the first point in this vector to be start point.
+    amp::PathPoints path_points;
+    path_points.x = x_bug;
+    path_points.y = y_bug;
+    path_points.step = 0;
+    bug_path.push_back(path_points);
+
+    // Set an iterator.
+    int iter = 0;
+    // Set the value for sizes of steps to take toward the goal with each move.
+    double step_size = 0.02;
+    // Set the distance to check ahead of the bug at each step to avoid obstacle collision.
+    double check_distance = 0.25;
+
+    // Create doubles to hold the x, y, and Euclidian distance to goal along with the angle to the goal.
+    double dx, dy, distance_to_goal, angle_to_goal;
+    // Create doubles to hold the step size taken in the x and y directions.
+    double step_x, step_y;
+    double forward_check;
+    int follow_obstacle;
+
+    int hit_index;
+    int following = 0;
+
+    int num_angles = 20;
+    double ahead_x[num_angles];
+    double ahead_y[num_angles];
+    double angle_view, angle_start, angle_delta;
+
+    bool inside;
+
+    int test = 0;
+
+
+    bug_path[iter].x = 0.763675;
+    bug_path[iter].y = 5.14368;
+    bug_path[iter].x = 13;
+    bug_path[iter].y = 5;
+    amp::WallInfo closestWall = findWall(bug_path[iter].x,bug_path[iter].y, problem);
+
+    std::cout << "(x,y):   (" << bug_path[iter].x << "," << bug_path[iter].y << ")" << std::endl;
+    std::cout << "(x1,y1): (" << (closestWall.x1) << "," << (closestWall.y1) << ")" << std::endl;
+    std::cout << "(x2,y2): (" << (closestWall.x2) << "," << (closestWall.y2) << ")" << std::endl << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100000));
+    
+    // Bug 1 Algorithm
+    while (!goalReached(bug_path[iter].x,bug_path[iter].y, step_size, problem) && iter <= 10000) {
+        dx = problem.q_goal[0] - bug_path[iter].x;
+        dy = problem.q_goal[1] - bug_path[iter].y;
+        distance_to_goal = sqrt(pow(dx,2) + pow(dy,2));
+        bug_path[iter].goal_distance = distance_to_goal;
+        angle_to_goal = atan(dy/dx);
+
+        angle_view  = M_PI/2;
+        angle_start = angle_to_goal - angle_view/2;
+        angle_delta = angle_view / (num_angles - 1);
+
+        inside = false;
+        std::vector<amp::PolyInfo> nearbyPolygon;
+        nearbyPolygon.resize(num_angles);
+        amp::PolyInfo tempPolygon;
+        for (int i = 0; i < num_angles; i++) {
+            if (i*angle_delta <= M_PI/4) {
+                ahead_x[i] = bug_path[iter].x + check_distance;
+                ahead_y[i] = bug_path[iter].y + check_distance * tan(angle_start + i*angle_delta);
+            } else {
+                ahead_y[i] = bug_path[iter].y + check_distance;
+                ahead_x[i] = bug_path[iter].x + check_distance * tan(angle_start + i*angle_delta);
+            }
+            tempPolygon = nearPolygon(ahead_x[i],ahead_y[i],problem);
+            nearbyPolygon[i] = tempPolygon;
+            if (nearbyPolygon[i].inside) {
+                inside = true;
+                check_distance = check_distance*99/100;
+                following = 1;
+                hit_index = iter;
+            }
+        }
+
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        if (!inside && !following) {
+            step_x = step_size * cos(angle_to_goal);
+            step_y = step_size * sin(angle_to_goal);
+
+            path_points.x = bug_path[iter].x + step_x;
+            path_points.y = bug_path[iter].y + step_y;
+            path_points.step = step_size;
+            path_points.goal_distance = distance_to_goal;
+            bug_path.push_back(path_points);
+            iter++;
+        } else {
+            test++;
+
+            amp::WallInfo closestWall = findWall(bug_path[iter].x, bug_path[iter].y, problem);
+
+            // std::cout << "(x,y):   (" << bug_path[iter].x << "," << bug_path[iter].y << ")" << std::endl;
+            // std::cout << "(x1,y1): (" << (closestWall.x1) << "," << (closestWall.y1) << ")" << std::endl;
+            // std::cout << "(x2,y2): (" << (closestWall.x2) << "," << (closestWall.y2) << ")" << std::endl << std::endl;
+
+            double angle_along_wall = atan((closestWall.y1-closestWall.y2)/(closestWall.x1-closestWall.x2));
+            double angle_next = atan((closestWall.y0-closestWall.y1)/(closestWall.x0-closestWall.x1));
+            double internal_angle = abs(angle_along_wall - angle_next);
+            if (internal_angle > M_PI) {
+                internal_angle = internal_angle - M_PI;
+            }
+            double turn_angle = (2*M_PI - internal_angle) / 2;
+            
+            for (int i = 0; i < num_angles; i++) {
+                nearbyPolygon[i].inside = 0;
+            }
+
+            double end_x = closestWall.x1 + (check_distance-0.05) * cos(turn_angle);
+            double end_y = closestWall.y1 + (check_distance-0.05) * sin(turn_angle);
+
+            inside = false;
+
+            while(!inside && (bug_path[iter].x <= end_x) && (bug_path[iter].y <= end_y)) {
+                angle_start = angle_along_wall - angle_view/2;
+
+                inside = false;
+                for (int i = 0; i < num_angles; i++) {
+                    if (i*angle_delta <= M_PI/4) {
+                        ahead_x[i] = bug_path[iter].x + check_distance;
+                        ahead_y[i] = bug_path[iter].y + check_distance * tan(angle_start + i*angle_delta);
+                    } else {
+                        ahead_y[i] = bug_path[iter].y + check_distance;
+                        ahead_x[i] = bug_path[iter].x + check_distance * tan(angle_start + i*angle_delta);
+                    }
+                    tempPolygon = nearPolygon(ahead_x[i],ahead_y[i],problem);
+                    nearbyPolygon[i] = tempPolygon;
+                    if (nearbyPolygon[i].inside) {
+                        inside = true;
+                    }
+                }
+
+                // nearbyPolygon = nearPolygon(ahead_x,ahead_y,problem);
+
+                step_x = step_size * cos(angle_along_wall);
+                step_y = step_size * sin(angle_along_wall);
+
+                path_points.x = bug_path[iter].x + step_x;
+                path_points.y = bug_path[iter].y + step_y;
+                path_points.step = step_size;
+                path_points.goal_distance = distance_to_goal;
+                bug_path.push_back(path_points);
+                iter++;
+            }
+
+            // std::cout << (closestWall.x1) << std::endl;
+            // std::cout << (closestWall.y1) << std::endl;
+            // std::cout << (closestWall.x2) << std::endl;
+            // std::cout << (closestWall.y2) << std::endl;
+            // std::cout << (bug_path[iter].x <= end_x) << std::endl;
+            // std::cout << (bug_path[iter].x) << std::endl;
+            // std::cout << (end_x) << std::endl;
+            // std::cout << (bug_path[iter].y <= end_y) << std::endl;
+            // std::cout << (bug_path[iter].y) << std::endl;
+            // std::cout << (end_y) << std::endl << std::endl;
+            
+            if (test == 10) {
+                break;
+            }
+            //break;
+            
+            // if (closestWall.isVertex && ) {
+
+            // }
+
+            // path_points.x = 0.75;
+            // path_points.y = 5.25;
+            // bug_path.push_back(path_points);
+            // break;
+        }
+    }
+    
+    path_points.x = problem.q_goal[0];
+    path_points.y = problem.q_goal[1];
+    path_points.x = 10;
+    path_points.y = 10;
+    bug_path.push_back(path_points);
+
+    // path.waypoints.push_back(problem.q_init);
+    // path.waypoints.push_back(Eigen::Vector2d(1.0, 5.0));
+    // path.waypoints.push_back(Eigen::Vector2d(3.0, 9.0));
+    // path.waypoints.push_back(problem.q_goal);
+
+    for (int i = 0; i < bug_path.size(); i++) {
+        path.waypoints.push_back(Eigen::Vector2d(bug_path[i].x, bug_path[i].y));
+    }
 
     return path;
 }
--- ws/MyBugAlgorithm.h
+++ ws/MyBugAlgorithm.h
@@ -3,6 +3,39 @@
 #include "AMPCore.h"
 #include "hw/HW2.h"
 
+#include <thread>
+
+namespace amp {
+
+struct PathPoints {
+    double x;
+    double y;
+    double step;
+    double goal_distance = -1;
+};
+
+struct PolyInfo {
+    bool inside = false;
+    int obstacle = -1;
+};
+
+struct WallInfo {
+    bool isVertex = false;
+    double distance;
+    double obstacle;
+    double x_close = -1;
+    double y_close = -1;
+    double x0;
+    double y0;
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+    
+};
+
+}
+
 /// @brief Declare your bug algorithm class here. Note this class derives the bug algorithm class declared in HW2.h
 class MyBugAlgorithm : public amp::BugAlgorithm {
     public:
@@ -10,7 +43,183 @@ class MyBugAlgorithm : public amp::BugAlgorithm {
         virtual amp::Path2D plan(const amp::Problem2D& problem) const override;
 
         // Add any other methods here...
-    
+
+        bool goalReached(double x, double y, double step_size, const amp::Problem2D& problem) const {
+            double dx = problem.q_goal[0] - x;
+            double dy = problem.q_goal[1] - y;
+            double distance_to_goal = sqrt(pow(dx,2) + pow(dy,2));
+            return distance_to_goal < step_size;
+        }
+        
+        amp::PolyInfo nearPolygon(double x_bug, double y_bug, const amp::Problem2D& problem) const {
+            // Define ints to hold values for the number of obstacles in the environment and the number of 
+            // vertices for each of those obstacles
+            int num_obstacles = problem.obstacles.size();
+            int num_vertices;
+
+            amp::PolyInfo polygon;
+
+            double x1, y1, x2, y2;
+
+            for (int i = 0; i < num_obstacles; i++) {
+                num_vertices = problem.obstacles[i].verticesCCW().size();
+                polygon.obstacle = i;
+                for (int j = 0; j < num_vertices; j++) {
+                    x1 = problem.obstacles[i].verticesCCW()[j][0];
+                    y1 = problem.obstacles[i].verticesCCW()[j][1];
+                    x2 = problem.obstacles[i].verticesCCW()[j+1][0];
+                    y2 = problem.obstacles[i].verticesCCW()[j+1][1];
+                    if (j == num_vertices-1) {
+                        x2 = problem.obstacles[i].verticesCCW()[0][0];
+                        y2 = problem.obstacles[i].verticesCCW()[0][1];
+                    }
+                    if (y_bug > std::fmin(y1,y2)) {
+                        if (y_bug <= std::fmax(y1,y2)){
+                            if (x_bug <= std::fmax(x1,x2)) {
+                                // Compute the location of the x-intercept on the line
+                                double x_int = (x2-x1) * ((y_bug-y1) / (y2-y1)) + x1;
+                                if (x2 == x1 || x_bug <= x_int) {
+                                    polygon.inside = !polygon.inside;
+                                }
+                            }
+                        }
+                    }
+                } if (polygon.inside) return polygon;
+            }
+            return polygon;
+        }
+        
+        amp::WallInfo findWall(double x_bug, double y_bug, const amp::Problem2D& problem) const {
+            // Define ints to hold values for the number of obstacles in the environment and the number of 
+            // vertices for each of those obstacles
+            int num_obstacles = problem.obstacles.size();
+            int num_vertices;
+
+            double x0, y0, x1, y1, x2, y2;
+
+            std::vector<std::vector<amp::WallInfo>> walls;
+            walls.resize(num_obstacles);
+
+            amp::WallInfo temp_wall;
+
+            for (int i = 0; i < num_obstacles; i++) {
+                num_vertices = problem.obstacles[i].verticesCCW().size();
+                walls[i].resize(num_vertices);
+                for (int j = 0; j < num_vertices; j++) {
+                    x1 = problem.obstacles[i].verticesCCW()[j][0];
+                    y1 = problem.obstacles[i].verticesCCW()[j][1];
+                    if (j == num_vertices-1) {
+                        x2 = problem.obstacles[i].verticesCCW()[0][0];
+                        y2 = problem.obstacles[i].verticesCCW()[0][1];
+                    } else {
+                        x2 = problem.obstacles[i].verticesCCW()[j+1][0];
+                        y2 = problem.obstacles[i].verticesCCW()[j+1][1];
+                    }
+                    if (j == 0) {
+                        x0 = problem.obstacles[i].verticesCCW()[num_vertices-1][0];
+                        y0 = problem.obstacles[i].verticesCCW()[num_vertices-1][1];
+                    } else {
+                        x0 = problem.obstacles[i].verticesCCW()[j-1][0];
+                        y0 = problem.obstacles[i].verticesCCW()[j-1][1];
+                    }
+
+                    // std::cout << "(x ,y ): (" << x_bug << "," << y_bug << ")" << std::endl;
+                    // std::cout << "(x0,y0): (" << x0 << "," << y0 << ")" << std::endl;
+                    // std::cout << "(x1,y1): (" << x1 << "," << y1 << ")" << std::endl;
+                    // std::cout << "(x2,y2): (" << x2 << "," << y2 << ")" << std::endl << std::endl;
+
+                    int num_steps = 101;
+
+                    double x_step = (x2-x1) / (num_steps-1);
+                    double y_step = (y2-y1) / (num_steps-1);
+
+                    double x_new, y_new, distance;
+
+                    double closest_vertex = sqrt(pow(x_bug-x1,2) + pow(y_bug-y1,2));
+                    temp_wall.x_close = x1;
+                    temp_wall.y_close = y1;
+                    if (sqrt(pow(x_bug-x2,2) + pow(y_bug-y2,2)) < closest_vertex) {
+                        closest_vertex = sqrt(pow(x_bug-x2,2) + pow(y_bug-y2,2));
+                        temp_wall.x_close = x2;
+                        temp_wall.y_close = y2;
+                    }
+                    temp_wall.distance = closest_vertex;
+                    temp_wall.obstacle = i;
+                    temp_wall.x0 = x0;
+                    temp_wall.y0 = y0;
+                    temp_wall.x1 = x1;
+                    temp_wall.y1 = y1;
+                    temp_wall.x2 = x2;
+                    temp_wall.y2 = y2;
+
+                    for (int k = 1; k < num_steps-2; k++) {
+                        x_new = x1 + x_step*k;
+                        y_new = y1 + y_step*k;
+                        double dist = sqrt(pow(x_bug-x_new,2) + pow(y_bug-y_new,2));
+                        if (dist <= temp_wall.distance) {
+                            temp_wall.distance = dist;
+                            temp_wall.x_close = x_new;
+                            temp_wall.y_close = y_new;
+                        }
+                    }
+
+                    std::cout << "(x,y): (" << x_bug << "," << y_bug << ")" << std::endl;
+                    std::cout << "(x,y): (" << temp_wall.x_close << "," << temp_wall.y_close << ")" << std::endl << std::endl;
+
+                    temp_wall.isVertex = false;
+                    if (std::abs(temp_wall.distance - closest_vertex) < 0.001) {
+                        temp_wall.isVertex = true; 
+                    }
+                    walls[i][j] = temp_wall;
+
+                    // LOG(temp_wall.distance);
+                    // LOG(walls[i][j].distance);
+                    // LOG(temp_wall.x_close);
+                    // LOG(walls[i][j].x_close);
+                    // LOG(temp_wall.y_close);
+                    // LOG(walls[i][j].y_close);
+                    // LOG(temp_wall.x1);
+                    // LOG(walls[i][j].x1);
+                    // LOG(temp_wall.y1);
+                    // LOG(walls[i][j].y1);
+                    // LOG(temp_wall.x2);
+                    // LOG(walls[i][j].x2);
+                    // LOG(temp_wall.y2);
+                    // LOG(walls[i][j].y2);
+
+                    // if (i == 4 && j == 0) {
+                    //     std::this_thread::sleep_for(std::chrono::milliseconds(100000));
+                    // }
+                }
+            }
+            
+            int closest_obstacle = 0;
+            int closest_wall = 0;
+            double closest_distance = walls[0][0].distance;
+            
+            for (int i = 0; i < num_obstacles; i++) {
+                num_vertices = problem.obstacles[i].verticesCCW().size();
+                for (int j = 0; j < num_vertices; j++) {
+                    if (walls[i][j].distance < closest_distance) {
+                        // LOG(walls[i][j].distance);
+                        closest_obstacle = i;
+                        closest_wall = j;
+                    } else if (walls[i][j].distance == closest_distance && j == num_vertices - 1) {
+                        std::cout << "hello" << std::endl;
+                        closest_wall = num_vertices - 1;
+                    }
+                }
+            }
+            // LOG(walls[closest_obstacle][closest_wall].distance);
+            // std::cout << "closest wall: " << walls[0][0].x1 << std::endl;
+            // std::cout << "closest wall: " << walls[0][0].y1 << std::endl;
+            // std::cout << "closest wall: " << walls[0][0].x2 << std::endl;
+            // std::cout << "closest wall: " << walls[0][0].y2 << std::endl;
+            // std::cout << "closest wall: " << walls[0][3].distance << std::endl;
+            // std::cout << "closest wall: " << walls[0][0].distance << std::endl;
+            return walls[closest_obstacle][closest_wall];
+        }
+
     private:
         // Add any member variables here...
 };

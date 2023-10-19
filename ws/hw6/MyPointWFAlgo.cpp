194b83c48b1a833647380d0caf4ab8993018ca58
m-gerber <64030957+m-gerber@users.noreply.github.com>
2023-10-19 00:58:03 -0600

    first attempt at point wave front compiling

--- ws/hw6/MyPointWFAlgo.cpp
+++ ws/hw6/MyPointWFAlgo.cpp
@@ -1,5 +1,7 @@
 #include "MyPointWFAlgo.h"
 #include "MyGridCSpace.h"
+#include "tools/Path.h"
+#include <queue>
 
 /// @brief Create a discretized planning space of an environment (point agent). If you abstracted your GridCSpace2DConstructor, you may be
 /// able to use that code here to construct a discretized C-space for a point agent.
@@ -12,26 +14,73 @@ std::unique_ptr<amp::GridCSpace2D> amp::MyPointWFAlgo::constructDiscretizedWorks
     return ptr;
 }
 
+int amp::MyPointWFAlgo::isValid(std::pair<int, int> inds, std::vector<std::vector<amp::cell>> graph, const amp::GridCSpace2D& grid_cspace) {
+    if (inds.first < 0 || inds.second < 0 || inds.first > graph.size() || inds.second > graph[0].size()) return 0;
+    if (grid_cspace.operator()(inds.first,inds.second)) return -1;
+    if (graph[inds.first][inds.second].visited) return 0;
+    return 1;
+}
+
 amp::Path2D amp::MyPointWFAlgo::planInCSpace(const Eigen::Vector2d& q_init, const Eigen::Vector2d& q_goal, const amp::GridCSpace2D& grid_cspace) {
 
     std::pair<double, double> x0_bounds = grid_cspace.x0Bounds();
     std::pair<double, double> x1_bounds = grid_cspace.x1Bounds();
 
-    std::pair<int, int> num_cells = grid_cspace.size();
-    std::pair<int, int> goal_cell = grid_cspace.getCellFromPoint(q_goal[0], q_goal[1]);
-    std::pair<int, int> init_cell = grid_cspace.getCellFromPoint(q_init[0], q_init[1]);
+    std::pair<double, double> num_cells = grid_cspace.size();
+    std::pair<double, double> goal_cell = grid_cspace.getCellFromPoint(q_goal[0], q_goal[1]);
+    std::pair<double, double> init_cell = grid_cspace.getCellFromPoint(q_init[0], q_init[1]);
+
+    double cell_size_x0 = (x0_bounds.second-x0_bounds.first) / num_cells.first;
+    double cell_size_x1 = (x1_bounds.second-x1_bounds.first) / num_cells.second;
 
     std::cout << "goal: " << grid_cspace.operator()(goal_cell.first,goal_cell.second) << std::endl;
 
-    std::pair<int, int> curr_pos = goal_cell;
+    std::pair<double, double> curr_pos = goal_cell;
 
-    // Assuming we can loop over items
+    std::vector<std::vector<amp::cell>> graph;
+    graph.resize(num_cells.first);
+    for (int i = 0; i < num_cells.first; i++) graph[i].resize(num_cells.second);
+
+    graph[curr_pos.first][curr_pos.second].visited = 1;
+    graph[curr_pos.first][curr_pos.second].val = 2;
+
+    std::queue<std::pair<double, double>> q;
+    q.push(curr_pos);
+
+    int dx[] = { -1, 0, 1, 0 };
+    int dy[] = { 0, 1, 0, -1 };
+    std::pair<double, double> new_pos;
+
     while(curr_pos.first != init_cell.first && curr_pos.second != init_cell.second) {
+        curr_pos = q.front();
+        q.pop();
 
-        
-
+        for (int i = 0; i < 4; i++) {
+            new_pos.first = curr_pos.first + dx[i];
+            new_pos.second = curr_pos.second + dy[i];
+            if (isValid({new_pos.first, new_pos.second}, graph, grid_cspace) == 1) {
+                q.push({new_pos.first, new_pos.second});
+                graph[new_pos.first][new_pos.second].visited = true;
+                graph[new_pos.first][new_pos.second].val = graph[curr_pos.first][curr_pos.second].val + 1;
+            } else if (isValid({new_pos.first, new_pos.second}, graph, grid_cspace) == -1) {
+                graph[new_pos.first][new_pos.second].visited = true;
+                graph[new_pos.first][new_pos.second].val = 1;
+            }
+        }
     }
 
+    amp::Path2D path;
+    
+    while (curr_pos != goal_cell) {
+        path.waypoints.push_back(Eigen::Vector2d(curr_pos.first*cell_size_x0,curr_pos.second*cell_size_x1));
+        for (int i = 0; i < 4; i++) {
+            new_pos.first = curr_pos.first + dx[i];
+            new_pos.second = curr_pos.second + dy[i];
+            if (graph[new_pos.first][new_pos.second].val < graph[curr_pos.first][curr_pos.second].val) curr_pos = new_pos;
+        }
+    }
 
-    return amp::Path2D();
+    path.waypoints.push_back(Eigen::Vector2d(q_goal[0], q_goal[1]));
+
+    return path;
 }
--- ws/hw6/MyPointWFAlgo.h
+++ ws/hw6/MyPointWFAlgo.h
@@ -8,6 +8,11 @@
 
 namespace amp {
 
+struct cell {
+    bool visited = 0;
+    int val = 0;
+};
+
 class MyPointWFAlgo : public amp::PointWaveFrontAlgorithm {
     public:
         virtual std::unique_ptr<amp::GridCSpace2D> constructDiscretizedWorkspace(const amp::Environment2D& environment) override;
@@ -16,6 +21,8 @@ class MyPointWFAlgo : public amp::PointWaveFrontAlgorithm {
         virtual amp::Path2D plan(const amp::Problem2D& problem) override {
             return amp::Path2D();
         }
+
+        int isValid(std::pair<int, int> inds, std::vector<std::vector<cell>> graph, const amp::GridCSpace2D& grid_cspace);
 
         // You need to implement here
         virtual amp::Path2D planInCSpace(const Eigen::Vector2d& q_init, const Eigen::Vector2d& q_goal, const amp::GridCSpace2D& grid_cspace) override;

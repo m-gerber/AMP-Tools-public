e35846816fff8fe021e3ab356138cedc776efa60
m-gerber <64030957+m-gerber@users.noreply.github.com>
2023-11-05 12:04:49 -0700

    pre-merge commit

--- ws/hw7/main.cpp
+++ ws/hw7/main.cpp
@@ -6,21 +6,23 @@ int main(int argc, char** argv) {
 
     amp::RNG::seed(amp::RNG::randiUnbounded());
 
-    amp::main_helper helper;
+    amp::HW7::hint();
 
-    bool smoothing = 1;   // Run smoothing on the path results
-    int num_trials = 100; // Set the number of trials to run
+    // amp::main_helper helper;
 
-    bool plotE1    = 0;   // Plot exercise 1 questions
-    bool trialsE1  = 1;   // Run XX trials for benchmark
-    bool printE1   = 0;   // Print performance statistics
-    bool boxE1     = 1;   // Plot the boxplots for the trials
-    helper.runE1(plotE1, trialsE1, printE1, boxE1, smoothing, num_trials);
+    // bool smoothing = 1;   // Run smoothing on the path results
+    // int num_trials = 100; // Set the number of trials to run
 
-    bool plotE2    = 0;   // Plot exercise 1 questions
-    bool trialsE2  = 0;   // Run XX trials for benchmark
-    bool boxE2     = 0;   // Plot the boxplots for the trials
-    helper.runE2(plotE2, trialsE2, boxE2, smoothing, num_trials);
+    // bool plotE1    = 0;   // Plot exercise 1 questions
+    // bool trialsE1  = 1;   // Run XX trials for benchmark
+    // bool printE1   = 0;   // Print performance statistics
+    // bool boxE1     = 1;   // Plot the boxplots for the trials
+    // helper.runE1(plotE1, trialsE1, printE1, boxE1, smoothing, num_trials);
+
+    // bool plotE2    = 0;   // Plot exercise 1 questions
+    // bool trialsE2  = 0;   // Run XX trials for benchmark
+    // bool boxE2     = 0;   // Plot the boxplots for the trials
+    // helper.runE2(plotE2, trialsE2, boxE2, smoothing, num_trials);
 
     // amp::HW7::grade<amp::MyPRM2D, amp::MyGoalBiasRRT2D>("mage7128@colorado.edu", argc, argv);
 
